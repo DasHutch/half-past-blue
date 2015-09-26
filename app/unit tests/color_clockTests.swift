882bd4c3c3dd7a4b50834aefca52b1d918696bdd
Gregory Hutchinson <mr.gregoryhutchinson@gmail.com>
2015-09-26 12:32:37 -0700

    chore (git) added git ignore and updated the repo folder a little bit
    effectively needing to reconnect the folders on disk to in Xcode proj

---
+++ app/unit tests/color_clockTests.swift
@@ -0,0 +1,36 @@
+//
+//  color_clockTests.swift
+//  color clockTests
+//
+//  Created by Gregory Hutchinson on 9/24/15.
+//  Copyright © 2015 DasHutch Development. All rights reserved.
+//
+
+import XCTest
+@testable import color_clock
+
+class color_clockTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
