33a23eb3c7dee951c2061b2ae0a88c61821680f9
Gregory Hutchinson <mr.gregoryhutchinson@gmail.com>
2015-09-26 13:47:30 -0700

    fix (git) found the issue... weird. adding all src code... :/
    anyway... here is all the _src code...

---
+++ app/_src/Clock.swift
@@ -0,0 +1,82 @@
+//
+//  Clock.swift
+//  color clock
+//
+//  Created by Gregory Hutchinson on 9/25/15.
+//  Copyright © 2015 DasHutch Development. All rights reserved.
+//
+
+import Foundation
+
+class Clock: NSObject {
+    
+    var currentDate: NSDate {
+        get {
+            return NSDate()
+        }
+    }
+    
+    var hour: Int {
+        get {
+            let components = currentDateComponents()
+            return components.hour
+        }
+    }
+    
+    var minute: Int {
+        get {
+            let components = currentDateComponents()
+            return components.minute
+        }
+    }
+    
+    var second: Int {
+        get {
+            let components = currentDateComponents()
+            return components.second
+        }
+    }
+    
+    var currentTimeString: String {
+        get {
+            let dateString = dateFormatter.stringFromDate(currentDate)
+            return dateString
+        }
+    }
+    
+    var dateFormatter: NSDateFormatter {
+        //????: This is not getting called on didSet in the init func... why?!
+        didSet {
+            //dateFormatter.timeStyle = .MediumStyle
+            
+            //FIXME: Forcibly using 24h time
+            //when I really just want to remove AM/PM if 12h time
+            dateFormatter.dateFormat = "HH:mm:ss"
+            dateFormatter.dateStyle = .NoStyle
+            dateFormatter.timeZone = NSTimeZone.localTimeZone()
+            dateFormatter.locale = NSLocale.currentLocale()
+        }
+    }
+    
+    //MARK: - Lifecycle
+    override init() {
+        dateFormatter = NSDateFormatter()
+        
+        //dateFormatter.timeStyle = .MediumStyle
+        dateFormatter.dateStyle = .NoStyle
+        
+        //FIXME: Forcibly using 24h time
+        //when I really just want to remove AM/PM if 12h time
+        dateFormatter.dateFormat = "HH:mm:ss"
+        dateFormatter.timeZone = NSTimeZone.localTimeZone()
+        dateFormatter.locale = NSLocale.currentLocale()
+    }
+    
+    //MARK: - Private
+    private func currentDateComponents() -> NSDateComponents {
+        
+        let unitFlags: NSCalendarUnit = [.Hour, .Minute, .Second]
+        let components = NSCalendar.currentCalendar().components(unitFlags, fromDate: currentDate)
+        return components
+    }
+}
