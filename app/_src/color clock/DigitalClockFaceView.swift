de313552984146cc6c43834d534657602d16a433
Gregory Hutchinson <mr.gregoryhutchinson@gmail.com>
2015-09-28 11:38:34 -0700

    feat (code, ui) new digital clock face view

---
+++ app/_src/color clock/DigitalClockFaceView.swift
@@ -0,0 +1,21 @@
+//
+//  DigitalClockFaceView.swift
+//  color clock
+//
+//  Created by Gregory Hutchinson on 9/28/15.
+//  Copyright © 2015 DasHutch Development. All rights reserved.
+//
+
+import UIKit
+
+class DigitalClockFaceView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
