ed6a030b9db7c0b35ec224ba219baada6372b1e9
Gregory Hutchinson <mr.gregoryhutchinson@gmail.com>
2015-09-28 11:39:52 -0700

    chore (refactor) moved color for time to color manager, and digital clock face view implementation

--- app/_src/color clock/ColorClockViewController.swift
+++ app/_src/color clock/ColorClockViewController.swift
@@ -6,22 +6,14 @@
 //  Copyright © 2015 DasHutch Development. All rights reserved.
 //
 
-//TODO:
-// - Analog Clock Face
-// - Double Tap (Alert to educate on swapping digital / analog)
-// - ColorManager Class for handling color choices from a NSDate
-// - Use MonoSpace font -- DONE
-// - Hide Status Bar -- DONE
-// - Create Clock Model (refactor) -- DONE
-// - Write UI Tests
-// - Write Unit Tests
-
 import UIKit
 import QuartzCore
 
 class ColorClockViewController: UIViewController {
     
-    @IBOutlet weak var currentDateLabel: UILabel!
+    @IBOutlet weak var digitalClockFace: DigitalClockFaceView!
+    @IBOutlet weak var analogClockFace: AnalogClockFaceView!
+    
     @IBOutlet weak var colorView: UIView!
     
     lazy var displayLink = CADisplayLink()
@@ -39,103 +31,47 @@ class ColorClockViewController: UIViewController {
     }
     
     deinit {
-        displayLink.invalidate()
-        displayLink.removeFromRunLoop(NSRunLoop.mainRunLoop(), forMode: NSDefaultRunLoopMode)
+        deconfigAndStopDisplayLink()
     }
+    
+    //MARK: - IBAction(s)
+    
+    //MARK: - Gesture(s)
 
     // MARK: - Private
     private func configAndRunDisplayLink() {
-        
         //NOTE: Run our Clock from our display refresh rate vs NSTimer (more accurate)
         displayLink = CADisplayLink(target: self, selector: Selector("updateClock"))
         displayLink.addToRunLoop(NSRunLoop.mainRunLoop(), forMode: NSDefaultRunLoopMode)
     }
     
-    func updateClock() {
-        
-        updateCurrentDateLabel(clock.currentTimeString)
+    private func deconfigAndStopDisplayLink() {
+        displayLink.invalidate()
+        displayLink.removeFromRunLoop(NSRunLoop.mainRunLoop(), forMode: NSDefaultRunLoopMode)
+    }
+    
+    //NOTE: As this is being called by displayLink,
+    //      we can't mark as private, even though is should be
+    internal func updateClock() {
         updateColorsForHours(clock.hour, minutes: clock.minute, seconds: clock.second)
     }
     
-    private func updateCurrentDateLabel(dateString: String) {
-        currentDateLabel.text = dateString
-    }
-    
     private func updateColorsForHours(hours: Int, minutes: Int, seconds: Int) {
+        let colorForTime = ColorManager().colorForHours(hours, minutes: minutes, seconds: seconds)
         
-        //TODO: Refactor to ColorManager?
-        let hueFromHours = adjustedHueValueFromHour(hours)
-        let brightnessFromMinutes = adjustedBrightnessValueFromMinute(minutes)
-        let saturationFromSeconds = adjustedSaturationValueFromSecond(seconds)
-        
-        let colorForTime = UIColor(hue: hueFromHours, saturation: saturationFromSeconds, brightness: brightnessFromMinutes, alpha: 1.0)
         updateUIWithColor(colorForTime)
+        updateDigitalClockFaceUI(forColor: colorForTime)
     }
     
-    //TODO: Refactor to ColorManager?
-    private func adjustedHueValueFromHour(currentHour: Int) -> CGFloat {
+    private func updateDigitalClockFaceUI(forColor color: UIColor) {
+        digitalClockFace.updateCurrentHours(clock.hour, minutes: clock.minute, seconds: clock.second)
+        digitalClockFace.updateCurrentTimeString(clock.currentTimeString)
         
-        let hoursDivisor: CGFloat = 24.0
-        
-        //NOTE: adjust upper/lower bounds to provide full 'hue' spectrum change across the day
-        let upperClampBound: CGFloat = 1.0
-        let lowerClampBound: CGFloat = 0.0
-        
-        //Hours (12) //NOTE: Will need to handle 24 or 12 for different locales
-        var clampedHours = CGFloat(currentHour) / hoursDivisor
-        if clampedHours > 1.0 || clampedHours < 0.0 {
-            clampedHours = clamp(clampedHours, lower: lowerClampBound,  upper: upperClampBound)
-        }
-        
-        return clampedHours
-    }
-    
-    //TODO: Refactor to ColorManager?
-    private func adjustedBrightnessValueFromMinute(currentMinute: Int) -> CGFloat {
-        
-        let minutesDivisor: CGFloat = 60.0
-        
-        let upperClampBound: CGFloat = 0.75
-        let lowerClampBound: CGFloat = 0.25
-
-        //Minutes (60)
-        var clampedMinutes = CGFloat(currentMinute) / minutesDivisor
-        if clampedMinutes > upperClampBound || clampedMinutes < lowerClampBound {
-            clampedMinutes = clamp(clampedMinutes, lower: lowerClampBound,  upper: upperClampBound)
-        }
-        
-        return clampedMinutes
-    }
-    
-    //TODO: Refactor to ColorManager?
-    private func adjustedSaturationValueFromSecond(currentSecond: Int) -> CGFloat {
-        
-        let secondsDivisor: CGFloat = 60.0
-        
-        //NOTE: Saturation - change upperBound (allow more saturation while still maintaining similar lowerBound clamp
-        let upperClampBound: CGFloat = 0.9
-        let lowerClampBound: CGFloat = 0.25
-
-        //Seconds (60)
-        var clampedSeconds = CGFloat(currentSecond) / secondsDivisor
-        
-        if clampedSeconds > upperClampBound || clampedSeconds < lowerClampBound {
-            clampedSeconds = clamp(clampedSeconds, lower: lowerClampBound,  upper: upperClampBound)
-        }
-        
-        return clampedSeconds
+        digitalClockFace.updateDigitalClockFontColor(color)
     }
     
     private func updateUIWithColor(color: UIColor) {
-        
-        updateDateLabelTextColorBasedOnColor(color)
         updateColorViewColor(color)
-    }
-
-    private func updateDateLabelTextColorBasedOnColor(color: UIColor) {
-
-        //NOTE: Update our Label Color based on BG color brightness
-        currentDateLabel.textColor = ColorManager().contrastingFontColorForColor(color)
     }
     
     private func updateColorViewColor(color: UIColor) {
