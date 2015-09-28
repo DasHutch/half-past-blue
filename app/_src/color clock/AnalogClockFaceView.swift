06aad611b7eb2668adc2b9ca1c76c9d2e2b29ad3
Gregory Hutchinson <mr.gregoryhutchinson@gmail.com>
2015-09-28 11:40:15 -0700

    feat (code, ui) new analog clock face view started

---
+++ app/_src/color clock/AnalogClockFaceView.swift
@@ -0,0 +1,13 @@
+//
+//  AnalogClockFaceView.swift
+//  color clock
+//
+//  Created by Gregory Hutchinson on 9/28/15.
+//  Copyright © 2015 DasHutch Development. All rights reserved.
+//
+
+import UIKit
+
+class AnalogClockFaceView: UIView {
+    
+}
