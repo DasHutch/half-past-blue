07f03ee1dee6d5418633ae78b56a7821c47f1eea
Gregory Hutchinson <mr.gregoryhutchinson@gmail.com>
2015-09-28 18:01:35 -0700

    chore (refactor) update to use digitalClockFace & analogClockFace

--- app/_src/color clock/ColorClockViewController.swift
+++ app/_src/color clock/ColorClockViewController.swift
@@ -22,7 +22,6 @@ class ColorClockViewController: UIViewController {
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
         configAndRunDisplayLink()
     }
     
@@ -35,8 +34,12 @@ class ColorClockViewController: UIViewController {
     }
     
     //MARK: - IBAction(s)
+    @IBAction func handleDoubleTap(sender: UITapGestureRecognizer) {
+        swapClockFaceTypes()
+    }
     
     //MARK: - Gesture(s)
+        //NOTE: Handled with IBAction & doesn't need a delegate
 
     // MARK: - Private
     private func configAndRunDisplayLink() {
@@ -61,20 +64,59 @@ class ColorClockViewController: UIViewController {
         
         updateUIWithColor(colorForTime)
         updateDigitalClockFaceUI(forColor: colorForTime)
+        updateAnalogClockFaceUI(forColor: colorForTime)
     }
     
     private func updateDigitalClockFaceUI(forColor color: UIColor) {
         digitalClockFace.updateCurrentHours(clock.hour, minutes: clock.minute, seconds: clock.second)
         digitalClockFace.updateCurrentTimeString(clock.currentTimeString)
-        
         digitalClockFace.updateDigitalClockFontColor(color)
     }
     
-    private func updateUIWithColor(color: UIColor) {
-        updateColorViewColor(color)
+    private func updateAnalogClockFaceUI(forColor color: UIColor) {
+        analogClockFace.updateCurrentHours(clock.hour, minutes: clock.minute, seconds: clock.second)
+        analogClockFace.updateAnalogClockFaceAndHandsColor(color)
     }
     
-    private func updateColorViewColor(color: UIColor) {
+    private func updateUIWithColor(color: UIColor) {
         colorView.backgroundColor = color
+    }
+
+    //MARK: Animation(s)
+    private func swapClockFaceTypes() {
+        
+        let animationDuration: NSTimeInterval = 0.5
+        let animationDelay: NSTimeInterval = 0.0
+        let animationOption: UIViewAnimationOptions = UIViewAnimationOptions.CurveEaseIn
+        
+        let visibleAlpha: CGFloat = 1.0
+        let hiddenAlpha: CGFloat = 0.0
+        
+        if digitalClockFace.hidden {
+            
+            UIView.animateWithDuration(animationDuration, delay: animationDelay, options: animationOption, animations: { () -> Void in
+                
+                self.digitalClockFace.alpha = visibleAlpha
+                self.analogClockFace.alpha = hiddenAlpha
+                
+            }, completion: { (finished) -> Void in
+                
+                self.digitalClockFace.hidden = !self.digitalClockFace.hidden
+                self.analogClockFace.hidden = !self.analogClockFace.hidden
+            })
+
+        }else {
+            
+            UIView.animateWithDuration(animationDuration, delay: animationDelay, options: animationOption, animations: { () -> Void in
+                
+                self.digitalClockFace.alpha = hiddenAlpha
+                self.analogClockFace.alpha = visibleAlpha
+                
+            }, completion: { (finished) -> Void in
+                
+                self.digitalClockFace.hidden = !self.digitalClockFace.hidden
+                self.analogClockFace.hidden = !self.analogClockFace.hidden
+            })
+        }
     }
 }
