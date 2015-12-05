7ad48206533aa55c955a5ff9578b5c950c877a39
Gregory Hutchinson <mr.gregoryhutchinson@gmail.com>
2015-12-05 15:21:38 -0800

    chore (refactor) forgot to include this on last commit... separate file for these constants

---
+++ app/_src/Segues.swift
@@ -0,0 +1,16 @@
+//
+//  Segues.swift
+//  color clock
+//
+//  Created by Gregory Hutchinson on 12/5/15.
+//  Copyright © 2015 DasHutch Development. All rights reserved.
+//
+
+import Foundation
+
+/** Segues Struct
+
+*/
+struct Segues {
+
+}
